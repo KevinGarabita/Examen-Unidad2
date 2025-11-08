e8465fb580a39d72b235f6306f58a632af9af17b
KevinGarabita <kevingarabita0@outlook.com>
2025-11-07 22:22:49 -0600

    avance 2
    se hicieron todas las validaciones para crear la compra y tambien los querys para insertar datos en la base de datos

--- index.js
+++ index.js
@@ -19,11 +19,17 @@ app.post("/api/purchases", async (req,res) => {
     if(details.length == 0){
         return res.status(400).json({message: "Debe de haber almenos un producto"})
     }
+
+    if(details.length > 5){
+        return res.status(400).json({message: "no puede haber mas de 5 productos"})
+    }
+
+    
     try{
         await connection.beginTransaction()
 
-        const[resultPurchase] = await connection.query("INSERT INTO purchases (user_id) VALUES (?)",
-            [user_id]
+        const[resultPurchase] = await connection.query("INSERT INTO purchases (user_id, total, status) VALUES (?, 0, ?)",
+            [user_id,status]
         )
 
         const purchase_id = resultPurchase.insertId
@@ -46,19 +52,31 @@ app.post("/api/purchases", async (req,res) => {
                 if(producto.stock < quantity){
                     return res.status(409).json({message: "no hay stock suficiente del producto " + product_id});
                 }
-                subtotal = price * quantity
-                await connection.query("INSERT INTO purchase_details (purhase_id, product_id, quantity, price, subtotal) VALUES (?, ?, ?, ?, ?)",
+                const subtotal = price * quantity
+                await connection.query("INSERT INTO purchase_details (purchase_id, product_id, quantity, price, subtotal) VALUES (?, ?, ?, ?, ?)",
                     [purchase_id,product_id,quantity,price,subtotal]
                 );
 
                 await connection.query("UPDATE products SET stock = stock - ? WHERE id = ?", 
                     [quantity,product_id]
                 );
-                await connection.query("UPDATE purchase");
                 total = total + price * quantity;
+                if(total > 3500){
+                    return res.status(409).json({message: "el total de la compra sobrepaso 3500"})
+                }
             }
-        } catch(err){
+        
+        const purchase_date = new Date();
+        await connection.query("UPDATE purchases SET total = ?, purchase_date = ? WHERE id = ?",
+            [total,purchase_date,purchase_id]
+        );
 
+        await connection.commit();
+
+        res.status(201).json({message:"compra creada con exito", id: purchase_id, Total: total})
+    } catch(err){
+        await connection.rollback();
+        res.status(500).json({error: "Error al crear la compra", detalle: err.message})
     }
 })
 
